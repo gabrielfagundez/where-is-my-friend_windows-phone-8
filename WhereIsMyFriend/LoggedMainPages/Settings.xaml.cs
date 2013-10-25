d2f2c3ba86abf57c6545679531c6e46b2d491705
carmelabeiro <carmela.beiro@gmail.com>
2013-10-25 20:59:53 -0300

    Se agrego iniciar sesion, requests y settings

---
+++ WhereIsMyFriend/LoggedMainPages/Settings.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WhereIsMyFriend.Classes;
+
+namespace WhereIsMyFriend.LoggedMainPages
+{
+    public partial class Settings : PhoneApplicationPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private async void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            LoggedUser l = LoggedUser.Instance;
+            await l.LogOut();
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+
+        }
+
+      //private async void Click_Logout(object sender, RoutedEventArgs e)
+      //  {
+      //      LoggedUser l = LoggedUser.Instance;
+      //      await l.LogOut();
+      //      NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+      //  }
+    }
+}
