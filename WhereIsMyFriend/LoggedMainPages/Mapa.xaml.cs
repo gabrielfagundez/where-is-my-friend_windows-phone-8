cee25d281a75e1d4afec32abf66487cebbde149f
mlrodriguez <marciorodriguez91@gmail.com>
2013-11-02 17:02:05 -0200

    dejo como estaba conversion a double

--- WhereIsMyFriend/LoggedMainPages/Mapa.xaml.cs
+++ WhereIsMyFriend/LoggedMainPages/Mapa.xaml.cs
@@ -411,8 +411,8 @@ namespace WhereIsMyFriend.LoggedMainPages
                 int i = 0;
                 foreach (var friend in p.Amigos)
                 {
-                    double lat = Convert.ToDouble(friend.Latitude.Replace(".",","));
-                    double longi = Convert.ToDouble(friend.Longitude.Replace(".", ","));
+                    double lat = Convert.ToDouble(friend.Latitude);
+                    double longi = Convert.ToDouble(friend.Longitude);
 
                     ph.insert(i.ToString(), friend.Mail, new GeoCoordinate(lat, longi));
                     i++;
